a883dc83eb501e4312d7f7fb1e48197356d8b668
EXC46268 <sebastian.arrejin@globallogic.com>
2022-10-13 18:37:52 -0300

    AddStartAndCloseLocalServer

--- src/test/java/seleniumgluecode/Hooks.java
+++ src/test/java/seleniumgluecode/Hooks.java
@@ -3,6 +3,7 @@ package seleniumgluecode;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+
 import db.MongoDBHelper;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -13,8 +14,11 @@ import utilities.RestAssuredExtension;
 import utils.LogHelper;
 
 import java.io.IOException;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 
 public class Hooks {
@@ -24,29 +28,13 @@ public class Hooks {
 
     private static WebDriver driver;
     private DriverManager driverManager;
+    private Process pr;
 
 
     @Before("@browser")
     public void setUp()  {
 
-        try{
-            //if (System.getProperty("ambiente") == "local") {
-                String[] cmd = {
-
-                        "cd C:/Users/sebastian.arrejin/post-venta-movil",
-                        "npm run start"};
-
-                Runtime.getRuntime().exec(cmd);
-
-                Thread.sleep(20000);
-
-            //}
-            }catch(IOException e){
-            System.out.println("Estas codeando con los codos");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
+        initLocalServer();
 
         LOGGER.log(Level.INFO, "Iniciando ambiente: "+System.getProperty("ambiente"));
         LOGGER.log(Level.INFO, "Ejecutando setUp..");
@@ -72,19 +60,50 @@ public class Hooks {
 
 
     @After("@browser")
-    public void tearDown(Scenario scenario){
+    public void tearDown(Scenario scenario) {
+
+
         if(scenario.isFailed()){
             byte[] screenshot= ((TakesScreenshot)driverManager.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.embed(screenshot,"image/png");
         }
+
         driverManager.quitDriver();
         LOGGER.log(Level.INFO, "Cerrando WebDriver");
-
+        closeLocalServer();
+        this.pr.destroyForcibly();
     }
 
     public static WebDriver getDriver(){
 
         return driver;
+    }
+
+    public void initLocalServer() {
+        if (System.getProperty("ambiente").equalsIgnoreCase("local")) {
+            try {
+                String[] initServer = {"cmd.exe", "/c", "cd C:\\Users\\sebastian.arrejin\\post-venta-movil && npm run start"};
+                this.pr = Runtime.getRuntime().exec(initServer);
+                LOGGER.log(Level.INFO, "Starting Local server... ");
+
+                this.pr.wait(5000);
+            } catch (Exception e) {
+                LOGGER.log(Level.INFO, "Error starting local server..");
+            }
+        }
+    }
+
+    public void closeLocalServer() {
+        if (System.getProperty("ambiente").equalsIgnoreCase("local")) {
+            try {
+                String[] closeServer = {"cmd.exe", "/c", "taskkill /im node.exe /t /f\n"};
+                this.pr = Runtime.getRuntime().exec(closeServer);
+                this.pr.waitFor(5, SECONDS);
+                LOGGER.log(Level.INFO, "Closing Local server... ");
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "Error closing server... ");
+            }
+        }
     }
 
 }
