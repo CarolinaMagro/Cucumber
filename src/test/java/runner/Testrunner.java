fcb9501a81df14ce61cd263451fc6ce5126e8494
EXC46268 <sebastian.arrejin@globallogic.com>
2022-10-21 09:14:59 -0300

    FixReport

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -20,7 +20,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
         glue = {"glue"},
         plugin = {"json:test/report/cucumber_report.json"},
         snippets = SnippetType.CAMELCASE
-        ,tags = {"@rest"}
+        ,tags = {"@rest,@browser"}
 )
 
 public class Testrunner {
